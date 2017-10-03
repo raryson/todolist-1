31a9c44e59d022643c60046d60b227676b554160
Daniel <daniel@umbler.com>
2017-10-03 11:35:46 -0300

    update

--- app.js
+++ app.js
@@ -3,48 +3,54 @@ const app = express();
 const bodyParser = require('body-parser');
 const mongoClient = require('mongodb').MongoClient;
 
-app.use(bodyParser.urlencoded({ extended:true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.use(express.static('assets'));
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
 
-    mongoClient.connect('mongodb://dbdogs:w-h5U#Qz4{n@tatooine.mongodb.umbler.com:48145/dbdogs', (err, db)=>{
-        
-                if(!err){
-                    let todo = req.body.todo;
-                    let collection = db.collection('dogsCollection');
-                
-                    const find = collection.find();
+mongoClient.connect('mongodb://localhost/dbdogs', (err, db) => {
 
-                    find.toArray((err, results)=>{
+        if (!err) {
+            let todo = req.body.todo;
+            let collection = db.collection('dogsCollection');
 
-                        res.render('index',{list: results});
-                    });
-                }
-});
+            const find = collection.find();
 
-});
+            find.toArray((err, results) => {
 
-app.post('/', (req, res)=>{
-    
-    mongoClient.connect('mongodb://dbdogs:w-h5U#Qz4{n@tatooine.mongodb.umbler.com:48145/dbdogs', (err, db)=>{
-
-                if(!err){
-                    let todo = req.body.todo;
-                    let collection = db.collection('dogsCollection');
-                
-                    collection.insert({reminder : todo});
-                    res.redirect('index');
-                }
+                res.render('index', {
+                    list: results
+                });
             });
+        }
+        db.close();
+    });
 
-    
 });
 
-app.listen(3000, ()=>{
+app.post('/', (req, res) => {
+
+    mongoClient.connect('mongodb://localhost/dbdogs', (err, db) => {
+
+        if (!err) {
+            let todo = req.body.todo;
+            let collection = db.collection('dogsCollection');
+
+            collection.insert({
+                reminder: todo
+            });
+            
+        }
+        db.close();
+        })
+    res.sendStatus(200);
+});
+
+app.listen(3000, () => {
     console.log('Ouvindo na 3000');
 });
-
--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,30 +1,45 @@
-
-$("ul").on("click","li", function(){
-    $(this).toggleClass("completed");    
-    
-});
-
-$("ul").on("click","span",function(event){
-$(this).parent().fadeOut(500,function(){
-    $(this).remove();
-});
-event.stopPropagation;
+$("ul").on("click", "li", function () {
+    $(this).toggleClass("completed");
 
 });
 
-$("#textbox").on("keypress",function(event){
-    if(event.which === 13){
+$("ul").on("click", "span", function (event) {
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove();
+    });
+    event.stopPropagation;
+
+});
+
+$("#textbox").on("keypress", function (event) {
+    if (event.which === 13) {
         var newTodo = $(this).val();
         $(this).val("");
-        $.post('/', {todo: newTodo});
+        $.post('/', {
+            todo: newTodo
+        })
+        /*.done((result)=>{
+            $("<li class='disable-select'><span><i class='fa fa-trash' aria-hidden='true'></i></span>" + result.reminder + "</li>")
+        })*/
+
+        .done((result)=>{
+                    $("li:last").append("<li class='disable-select'><span><i class='fa fa-trash' aria-hidden='true'></i></span>" + result.newTodo + "</li>")
+                })
+
+
+        .fail((error)=>{throw error})
     };
 });
 
-$("i.fa-plus").on("click", function(){
-    var newTodo = $("#textbox").val();
-    $("#textbox").val("");
-    $.post('/', {todo: newTodo});
-});
-
-
+    $("i.fa-plus").on("click", function () {
+        var newTodo = $("#textbox").val();
+        $("#textbox").val("");
+        $.post('/', {
+                todo: newTodo
+        })
+        .done((result)=>{$("<li class='disable-select'><span><i class='fa fa-trash' aria-hidden='true'></i></span>" + result.newTodo + "</li>")
+        })
+        .fail((error)=>{throw error})
+    
+    });
 
