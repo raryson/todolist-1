1df5755d7b1c88cd91950057d4c7373291f34499
Daniel <daniel.gomes@redehost.com.br>
2017-10-04 12:57:59 -0300

    vai cla

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ mongoClient.connect('mongodb://dbdogs:w-h5U#Qz4{n@tatooine.mongodb.umbler.com:48
 });
 
 app.get('/bicca', (req, res)=> {
-	res.send('bicca');
+	res.render('bicca');
 });
 
 app.post('/', (req, res) => {
