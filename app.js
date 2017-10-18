6b7d858207b1eb8e0b2bb519bc80cf4254b49a74
Dan <danielbgomes21@gmail.com>
2017-10-18 11:59:50 -0200

    update nas rotas

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const mongoClient = require('mongodb').MongoClient;
-const ObjectId = require('mongodb').ObjectID;
+const   express = require('express');
+        app = express();
+        bodyParser = require('body-parser');
+        mongoClient = require('mongodb').MongoClient;
+        ObjectId = require('mongodb').ObjectID;
 
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('assets'));
 
@@ -15,11 +13,11 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
 
-    mongoClient.connect('mongodb://dbdogs:w-h5U#Qz4{n@tatooine.mongodb.umbler.com:48145/dbdogs', (err, db) => {
+    mongoClient.connect('url', (err, db) => {
 
         if (!err) {
             let todo = req.body.todo;
-            let collection = db.collection('dogsCollection');
+            let collection = db.collection('todoCollection');
 
             const find = collection.find();
 
@@ -29,57 +27,53 @@ app.get('/', (req, res) => {
                     list: results
                 });
             });
+        } else {
+            throw err    
+            db.close();
         }
-        db.close();
     });
-
 });
 
-app.post('/addTodo', (req, res) => {
+app.post('/adicionar', (req, res) => {
 
-    mongoClient.connect('mongodb://dbdogs:w-h5U#Qz4{n@tatooine.mongodb.umbler.com:48145/dbdogs', (err, db) => {
+    mongoClient.connect('url', (err, db) => {
 
         if (!err) {
             let todo = req.body.todo;
-            let collection = db.collection('dogsCollection');
+            let collection = db.collection('todoCollection');
 
             collection.insert({
                 reminder: todo
             });
-
+        } else {
+            throw err
+            db.close();
         }
-        db.close();
     })
     res.sendStatus(200);
 });
 
-app.post('/removeTodo', (req, res) => {
+app.post('/remover', (req, res) => {
 
-    mongoClient.connect('mongodb://dbdogs:w-h5U#Qz4{n@tatooine.mongodb.umbler.com:48145/dbdogs', (err, db) => {
+    mongoClient.connect('url', (err, db) => {
 
         if (!err) {
             let todo = req.body.removedTodo;
             let id = req.body.id;
 
 
-            let collection = db.collection('dogsCollection');
+            let collection = db.collection('todoCollection');
 
             collection.deleteOne({
                 _id: ObjectId(id)
             })
-
         } else {
             throw err
+            db.close();
         }
-        db.close();
-
-
     });
     res.sendStatus(200);
 });
-
-
-
 
 app.listen(3000, () => {
     console.log('Ouvindo na 3000');
--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,68 +1,57 @@
 
 $("ul").on("click", "li", function () {
     $(this).toggleClass("completed");
-
 });
-
-$("ul").on("click", "span", function (event) {
-
-    let id = $(this).attr('id');
-    console.log(id);
-    $.post('/removeTodo', {
-        id: id
-    })
-
-        .done((retorno) => {
-            $(this).parent().fadeOut(500, function () {
-                $(this).remove();
-
-            });
-            event.stopPropagation;
-        })
-
-
-        .fail((error) => { throw error })
-
-
-
-
-
-
-});
-
-
 
 $("#textbox").on("keypress", function (event) {
     if (event.which === 13) {
         let newTodo = $(this).val();
         if (newTodo.length <= 0) {
-
             alert("O valor da todo não pode ser vazio");
             return;
-        }
+        } else {
         $(this).val("");
-        $.post('/addTodo', {
+        $.post('/adicionar', {
             todo: newTodo
         })
-
-            .done(() => {
-                $("#list").append("<li class='disable-select'><span><i class='fa fa-trash' aria-hidden='true'></i></span>" + newTodo + "</li>")
-            })
-
-            .fail((error) => { throw error })
-    };
+        .done(() => {
+            $("#list").append("<li class='disable-select'><span><i class='fa fa-trash' aria-hidden='true'></i></span>" + newTodo + "</li>")
+        })
+        .fail((error) => { throw error })
+        };
+    }
 });
 
 $("i.fa-plus").on("click", function () {
-    var newTodo = $("#textbox").val();
+    let newTodo = $("#textbox").val();
+    if (newTodo.length <= 0) {
+        alert("O valor da todo não pode ser vazio");
+        return;
+    } else {
     $("#textbox").val("");
-    $.post('/addTodo', {
+    $.post('/adicionar', {
         todo: newTodo
     })
-        .done((result) => {
-            $("<li class='disable-select'><span><i class='fa fa-trash' aria-hidden='true'></i></span>" + result.newTodo + "</li>")
+        .done(() => {
+            $("#list").append("<li class='disable-select'><span><i class='fa fa-trash' aria-hidden='true'></i></span>" + newTodo + "</li>")
         })
         .fail((error) => { throw error })
+    } 
+});
 
+$("ul").on("click", "span", function (event) {
+    
+        let id = $(this).attr('id');
+    
+        $.post('/remover', {
+            id: id
+        })
+        .done((retorno) => {
+            $(this).parent().fadeOut(500, function () {
+                    $(this).remove();   
+             });
+            event.stopPropagation;
+        })
+        .fail((error) => { throw error })
 });
 
